70322ba415d31b682b7eae36371fe346df1fa655
Bipin Gouda <bipingouda1199@gmail.com>
2023-12-29 08:41:17 +0530

    update

---
+++ A2Z_DSA/Arrays/Striver/Moove zeroes to Left or Right.py
@@ -0,0 +1,22 @@
+#ie moove non zeroes to left so if 0 do nothing else swap
+def moveZerosright(n,a):
+    l=0
+    for r in range(n):
+        if a[r]:
+            a[l],a[r]=a[r],a[l]
+            l+=1
+    return a
+
+# if moove zeroes to left if 0 found swap it
+def moveZerosleft(n,a):
+    l=0
+    for r in range(n):
+        if a[r]==0:
+            a[l],a[r]=a[r],a[l]
+            l+=1
+    return a
+
+x=moveZerosright(7,[1,0,2,4,3,0,1])
+y=moveZerosleft(7,[1,0,2,4,3,0,1])
+print(x)
+print(y)
